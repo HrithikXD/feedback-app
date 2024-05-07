94501966fd874bedc8bc222c36b235fe555e6d66
GNRTRREX <hrithikhh86@gmail.com>
2024-05-07 15:36:42 +0530

    added jsonserver

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext,useState,useEffect } from "react";
-import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -60,7 +59,7 @@ export const FeedbackProvider = ({children})=>{
             item.id===id ? {...item, ...data} : item))
     }
 
-    
+
     return <FeedbackContext.Provider value={{feedback,feedbackEdit,isLoading,deleteFeedback, addFeedback, editFeedback, updateFeedback, }}>
         {children}
     </FeedbackContext.Provider>
